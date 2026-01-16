fb34a8ca4db59697c5319034887cd9a3a5503656
bobbyku0104 <143bobby123@gmail.com>
2026-01-17 01:30:19 +0530

    make video file page

---
+++ client/src/pages/Video.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../compoents/Navbar';
+import VideoPlayer from '../compoents/VideoOutput';
+
+export default function Video() {
+    return (    
+        <div>
+            <Navbar />
+            <VideoPlayer />
+        </div>
+    );
+}
