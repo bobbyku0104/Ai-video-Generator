570a385cb38e943dbefbd0fcaf7ba7ee4cce49fe
bobbyku0104 <143bobby123@gmail.com>
2026-01-17 01:29:45 +0530

    Add home section

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../compoents/Navbar";
+import Hero from "../compoents/Hero";
+import PromptBox from "../compoents/PromptBox";
+
+
+export default function Home() {
+  return (
+
+    <div>
+        <Navbar />
+        <Hero />
+        <PromptBox />
+        
+    </div>
+  );
+}
